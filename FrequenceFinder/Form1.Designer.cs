08eb43c4c415977370dd05ae6db301ed1125b627
cavejohnson332 <cavejohnson332@gmail.com>
2015-11-22 21:27:04 +0200

    WordFrequencyFinder
    WordFrequencyFinder is a simple application for getting frequency and
    count of a specific word from the text.

---
+++ FrequenceFinder/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace FrequenceFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.RichTextBox();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_frq = new System.Windows.Forms.Button();
+            this.btn_wordFrqnc = new System.Windows.Forms.Button();
+            this.textBox_word = new System.Windows.Forms.TextBox();
+            this.wordCount_label = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.totalCount = new System.Windows.Forms.Label();
+            this.frquency_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputBox.Location = new System.Drawing.Point(3, 3);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(331, 373);
+            this.InputBox.TabIndex = 0;
+            this.InputBox.Text = "";
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Open.Location = new System.Drawing.Point(340, 3);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(104, 53);
+            this.btn_Open.TabIndex = 1;
+            this.btn_Open.Text = "Open text file";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_frq
+            // 
+            this.btn_frq.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_frq.Location = new System.Drawing.Point(340, 62);
+            this.btn_frq.Name = "btn_frq";
+            this.btn_frq.Size = new System.Drawing.Size(104, 53);
+            this.btn_frq.TabIndex = 2;
+            this.btn_frq.Text = "Get all frequencies";
+            this.btn_frq.UseVisualStyleBackColor = true;
+            this.btn_frq.Click += new System.EventHandler(this.btn_frq_Click);
+            // 
+            // btn_wordFrqnc
+            // 
+            this.btn_wordFrqnc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_wordFrqnc.Location = new System.Drawing.Point(340, 235);
+            this.btn_wordFrqnc.Name = "btn_wordFrqnc";
+            this.btn_wordFrqnc.Size = new System.Drawing.Size(104, 53);
+            this.btn_wordFrqnc.TabIndex = 3;
+            this.btn_wordFrqnc.Text = "Get frequency of a specific form";
+            this.btn_wordFrqnc.UseVisualStyleBackColor = true;
+            this.btn_wordFrqnc.Click += new System.EventHandler(this.btn_wordFrqnc_Click);
+            // 
+            // textBox_word
+            // 
+            this.textBox_word.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_word.Location = new System.Drawing.Point(340, 294);
+            this.textBox_word.Name = "textBox_word";
+            this.textBox_word.Size = new System.Drawing.Size(101, 20);
+            this.textBox_word.TabIndex = 4;
+            // 
+            // wordCount_label
+            // 
+            this.wordCount_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.wordCount_label.AutoSize = true;
+            this.wordCount_label.Location = new System.Drawing.Point(340, 317);
+            this.wordCount_label.Name = "wordCount_label";
+            this.wordCount_label.Size = new System.Drawing.Size(66, 13);
+            this.wordCount_label.TabIndex = 5;
+            this.wordCount_label.Text = "Word count:";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(450, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(383, 379);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Word";
+            this.columnHeader1.Width = 163;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Count";
+            this.columnHeader2.Width = 91;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Frequency";
+            this.columnHeader3.Width = 123;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Save.Location = new System.Drawing.Point(340, 121);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(104, 53);
+            this.btn_Save.TabIndex = 7;
+            this.btn_Save.Text = "Save frequencies\r\nin a text file";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // totalCount
+            // 
+            this.totalCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalCount.AutoSize = true;
+            this.totalCount.Location = new System.Drawing.Point(340, 177);
+            this.totalCount.Name = "totalCount";
+            this.totalCount.Size = new System.Drawing.Size(90, 13);
+            this.totalCount.TabIndex = 8;
+            this.totalCount.Text = "Total word count:";
+            // 
+            // frquency_label
+            // 
+            this.frquency_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.frquency_label.AutoSize = true;
+            this.frquency_label.Location = new System.Drawing.Point(340, 339);
+            this.frquency_label.Name = "frquency_label";
+            this.frquency_label.Size = new System.Drawing.Size(60, 13);
+            this.frquency_label.TabIndex = 9;
+            this.frquency_label.Text = "Frequency:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 379);
+            this.Controls.Add(this.frquency_label);
+            this.Controls.Add(this.totalCount);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.wordCount_label);
+            this.Controls.Add(this.textBox_word);
+            this.Controls.Add(this.btn_wordFrqnc);
+            this.Controls.Add(this.btn_frq);
+            this.Controls.Add(this.btn_Open);
+            this.Controls.Add(this.InputBox);
+            this.MinimumSize = new System.Drawing.Size(700, 417);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word Frequency Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox InputBox;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Button btn_frq;
+        private System.Windows.Forms.Button btn_wordFrqnc;
+        private System.Windows.Forms.TextBox textBox_word;
+        private System.Windows.Forms.Label wordCount_label;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Label totalCount;
+        private System.Windows.Forms.Label frquency_label;
+    }
+}
+
---
+++ FrequenceFinder/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace FrequenceFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] allWords;
+        List<string> words = new List<string>();
+        List<int> WordsCountList = new List<int>();
+
+        int sortColumn = 1;
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            // creating open dialog
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = ".txtFile|*.txt";
+            dialog.Title = "Select text file";
+
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                InputBox.Text = String.Empty; //reset
+                try
+                {
+                    InputBox.Text = File.ReadAllText(dialog.FileName, Encoding.Default);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "An error occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void btn_frq_Click(object sender, EventArgs e)
+        {
+            if (InputBox.Text != string.Empty && InputBox.Text.Contains(" "))
+            {
+                //reset everything to default
+                words.Clear();
+                WordsCountList.Clear();
+                listView1.Items.Clear();
+
+                //preparing the text for getting better results
+                allWords = RemovePunctuationNSplit(InputBox.Text);
+
+                //comparing all words
+                #region Core algorithm
+
+                for (int i = 0; i < allWords.Length; i++)
+                {
+                    //if the word is not in the list -- add it
+                    if (!words.Contains(allWords[i]))
+                    {
+                        words.Add(allWords[i]);
+                        WordsCountList.Add(1);
+                    }
+                    else //else increase its count number
+                    {
+                        for (int k = 0; k < words.Count; k++)
+                        {
+                            if (words[k] == allWords[i]) WordsCountList[k]++;
+                        }
+                    }
+                }//end for
+                #endregion
+
+                totalCount.Text = "Total word count:\r\n" + allWords.Length;
+
+                //showing results in the listView
+                for (int i = 0; i < words.Count; i++)
+                {
+                    //adding word to the listViewItem
+                    ListViewItem item = new ListViewItem(words[i]);
+
+                    //adding number of occurrencies to the listViewItem
+                    item.SubItems.Add(WordsCountList[i].ToString());
+
+                    //adding frequency to the listViewItem
+                    float frequency = (float)WordsCountList[i] / (float)allWords.Length;
+                    item.SubItems.Add(frequency.ToString());
+
+                    //adding item to the listView
+                    listView1.Items.Add(item);
+                }
+
+                //sorting by frequency
+                this.listView1.ListViewItemSorter = new ListViewItemComparer(sortColumn, SortOrder.Descending);
+                // Call the sort method to manually sort.
+                listView1.Sort();
+
+
+            }
+            else MessageBox.Show("There is no textin the field.", "Word Frequency Finder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                // creating save dialog
+                SaveFileDialog dialog = new SaveFileDialog();
+
+                dialog.Filter = ".txt File|*.txt";
+                dialog.Title = "Save TXT file";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    List<string> frequencies = new List<string>();
+
+                    //adding some commentaries
+                    frequencies.Add("# Compiled with Word Frequency Finder at" + DateTime.Now.ToString("H:mm dd/mm/yyyy"));
+                    frequencies.Add("# Total word count: " + allWords.Length);
+                    frequencies.Add(string.Format("{0,-18} {1,-10} {2}", "WORD", "COUNT", "FREQUENCY"));
+
+                    for (int i = 0; i < listView1.Items.Count; i++)
+                    {
+                        //adding item information
+                        string itemStr = string.Format("{0,-18} {1,-10} {2}", listView1.Items[i].Text,
+                            listView1.Items[i].SubItems[1].Text, listView1.Items[i].SubItems[2].Text);
+                        frequencies.Add(itemStr);
+                    }
+
+                    File.WriteAllLines(dialog.FileName, frequencies.ToArray());
+
+                    MessageBox.Show("Operation completed!", "Word Frequency Finder", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else MessageBox.Show("No word information!", "Word Frequency Finder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btn_wordFrqnc_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(InputBox.Text) && InputBox.Text.Contains(" "))
+            {
+                int wordCount = 0;
+
+                //preparing the text for getting better results
+                allWords = RemovePunctuationNSplit(InputBox.Text);
+
+                // if the word is the one we are looking for -- increase its count number
+                for (int i = 0; i < allWords.Length; i++)
+                {
+                    if (allWords[i] == textBox_word.Text) wordCount++;
+                }
+
+                //calculating the frequency
+                float frqnc = (float)wordCount / (float)allWords.Length;
+
+                //showing the information
+                wordCount_label.Text = "Word count: " + wordCount;
+                totalCount.Text = "Total word count:\r\n" + allWords.Length;
+                frquency_label.Text = "Frequency is:\r\n" + frqnc;
+            }
+            else MessageBox.Show("There is no text.", "Word Frequency Finder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private string[] RemovePunctuationNSplit(string text)
+        {
+            //one way to ignore upercase is to make everything lovercase
+            string temp = text.ToLower();
+
+            //getting rid of punctuation
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == '’' || temp[i] == '\'')
+                {
+                    //keeping apostrophe sign
+                }
+                else if (char.IsPunctuation(temp[i]) || temp[i] == '\"')
+                {
+                    //one way to avoid messing with the length of the
+                    //array is to replace everything with whitespaces
+                    //and split method will take care of them 
+                    temp = temp.Replace(temp[i], ' ');
+                }
+            }
+            return temp.Split(new string[] { "\n", " " }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        //based on the MSDN manual on how to sort a listview column
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine whether the column is the same as the last column clicked.
+            if (e.Column != sortColumn)
+            {
+                // Set the sort column to the new column.
+                sortColumn = e.Column;
+                // Set the sort order to ascending by default.
+                listView1.Sorting = SortOrder.Ascending;
+            }
+            else
+            {
+                // Determine what the last sort order was and change it.
+                if (listView1.Sorting == SortOrder.Ascending)
+                    listView1.Sorting = SortOrder.Descending;
+                else
+                    listView1.Sorting = SortOrder.Ascending;
+            }
+            // Set the ListViewItemSorter property to a new ListViewItemComparer object.
+            this.listView1.ListViewItemSorter = new ListViewItemComparer(e.Column, listView1.Sorting);
+            // Call the sort method to manually sort.
+            listView1.Sort();
+
+        }
+    }
+
+    //based on the MSDN manual on how to sort a listview column
+    class ListViewItemComparer : IComparer
+    {
+        private int col;
+        private SortOrder order;
+
+        //ctor with two parameters
+        public ListViewItemComparer(int column, SortOrder order)
+        {
+            col = column;
+            this.order = order;
+        }
+        public int Compare(object x, object y)
+        {
+            int returnVal;
+            // Determine whether the type being compared is a number.
+            if (col == 1 || col == 2)
+            {
+                decimal first = decimal.Parse(((ListViewItem)x).SubItems[col].Text);
+                decimal second = decimal.Parse(((ListViewItem)y).SubItems[col].Text);
+                returnVal = decimal.Compare(first, second);
+            }// If neither compared object has a valid number format, compare as a string.
+            else
+            {
+                // Compare the two items as a string.
+                returnVal = String.Compare(
+                    ((ListViewItem)x).SubItems[col].Text,
+                    ((ListViewItem)y).SubItems[col].Text);
+            }
+            // Determine whether the sort order is descending.
+            //If positive Invert the value returned by String.Compare.
+            if (order == SortOrder.Descending) returnVal *= -1;
+
+            return returnVal;
+        }
+    }
+
+}
